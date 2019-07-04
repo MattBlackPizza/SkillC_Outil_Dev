08a1a6b603705827734764ba6b6047d655de00f1
MattBlackPizza <45101881+MattBlackPizza@users.noreply.github.com>
2019-07-04 18:27:40 +0200

    Update size-l.php

--- size-l/size-l.php
+++ size-l/size-l.php
@@ -4,7 +4,6 @@
   <?php include 'player-line-up-content.php'; ?>
   <?php include 'menu.php'; ?>
 </section>
-<!-- Écran 1 - Nav TimeLine -->
 
 <!-- Écran 2 - Nav Classique -->
 <section class="stadium-size-l size-l size-l-view">
@@ -12,35 +11,30 @@
   <?php include 'player-line-up-content.php'; ?>
   <?php include 'menu.php'; ?>
 </section>
-<!-- Écran 2 - Nav Classique -->
 
 <!-- Écran 3 - Front View -->
 <section class="stadium-size-l-front-view size-l size-l-view">
   <?php include 'nav.php'; ?>
   <?php include 'menu.php'; ?>
 </section>
-<!-- Écran 3 - Front View -->
 
 <!-- Écran 4 - Stats -->
 <section class="stadium-size-l size-l">
   <?php include 'stats.php'; ?>
   <?php include 'menu.php'; ?>
 </section>
-<!-- Écran 4 - Stats -->
 
 <!-- Écran 5 - Match Sheet -->
 <section class="stadium-size-l size-l">
   <?php include 'match-sheet.php'; ?>
   <?php include 'menu.php'; ?>
 </section>
-<!-- Écran 5 - Match Sheet -->
 
 <!-- Écran 6 - Line Up -->
 <section class="stadium-size-l size-l">
   <?php include 'line-up.php'; ?>
   <?php include 'menu.php'; ?>
 </section>
-<!-- Écran 6 - Line Up -->
 
 <!-- Écran 7 - Notification(s) -->
 <section class="stadium-size-l size-l">
@@ -48,7 +42,6 @@
   <?php include 'notification.php'; ?>
   <?php include 'nav.php'; ?>
 </section>
-<!-- Écran 7 - Notification(s) -->
 
 <!-- Écran 8 - Message Alert -->
 <section class="stadium-size-l size-l">
@@ -56,4 +49,3 @@
   <?php include 'alert.php'; ?>
   <?php include 'menu.php'; ?>
 </section>
-<!-- Écran 8 - Message Alert -->
