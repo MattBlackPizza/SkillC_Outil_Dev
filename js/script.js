98905e351131adc97a02aaef25f415bc55643e0d
MattBlackPizza <45101881+MattBlackPizza@users.noreply.github.com>
2019-07-05 11:55:06 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,39 @@
 $(function() {
    $('.tab-menu').click(function() {
-       $('.tab-menu').addClass('tab-menu-active');
-       $(this).removeClass('tab-menu-active');
+       $('.tab-menu').removeClass('tab-menu-active');
+       $(this).addClass('tab-menu-active');
    });
 });
 
 $(document).ready(function(){
   setTimeout(function(){$(".notifications").addClass("animate");}, 500);
+});
+
+$(function() {
+   $('.icon-match-infos').click(function() {
+       $('.icon-match-infos').addClass('hide');
+       $('.icon-match-infos-close').removeClass('hide');
+       $('.informations').addClass('hide');
+       $('.view-icon').addClass('hide');
+       $('.menu').addClass('hide');
+       $('.match-infos-box').removeClass('hide');
+       $('.notifications').addClass('hide');
+       $('.player-line-up-content').addClass('hide');
+       $('.time-line-content').addClass('hide');
+       $('.match-infos-box').addClass('fadeIn');
+   });
+});
+
+$(function() {
+   $('.icon-match-infos-close').click(function() {
+       $('.icon-match-infos').removeClass('hide');
+       $('.icon-match-infos-close').addClass('hide');
+       $('.informations').removeClass('hide');
+       $('.view-icon').removeClass('hide');
+       $('.menu').removeClass('hide');
+       $('.match-infos-box').addClass('hide');
+       $('.notifications').removeClass('hide');
+       $('.player-line-up-content').removeClass('hide');
+       $('.time-line-content').removeClass('hide');
+   });
 });
