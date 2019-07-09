1e09ab035e5d31eab3e3ce23c7762c7d24b58913
MattBlackPizza <45101881+MattBlackPizza@users.noreply.github.com>
2019-07-09 16:09:29 +0200

    Add files via upload

--- size-l/nav-timeline.php
+++ size-l/nav-timeline.php
@@ -2,6 +2,16 @@
   <div class="view-icon">
     <img src="assets/icons/icon-view.svg">
   </div>
+  <div class="score-box">
+    <div class="score-content score-content-nav-timeline">
+      <img class="shape-team-nav-timeline" src="../assets/shapes/players/shape-team-a.svg">
+      <span class="score">PCF 1</span>
+    </div>
+    <div class="score-content">
+      <img class="shape-team-nav-timeline" src="../assets/shapes/players/shape-team-b.svg">
+      <span class="score">RCL 1</span>
+    </div>
+  </div>
   <div class="time-line-content">
     <?php include 'timeline-data.php'; ?>
     <div class="time-line-minutes">
--- size-l/notification.php
+++ size-l/notification.php
@@ -1,11 +1,25 @@
-<div class="notifications notifs-xy-a">
-  <div class="triangle-bg"></div>
-  <img class="shape-notifs" src="../assets/shapes/notifications/shape-team-a.svg" />
-  <div class="times-notifs color-team-a">
-    <span>75'</span>
+<div class="notifications-content">
+
+  <div class="animated delay-2s fadeIn notifications notifs-xy-a">
+    <div class="times-notifs color-team-a">
+      <span>75'</span>
+    </div>
+    <div class="info-notifs">
+      <span class="notifs-action">Arrêt gardien</span><br>
+      <span class="notifs-name-player">Areola,</span> <span class="notifs-name-team">Paris SG</span>
+    </div>
   </div>
-  <div class="info-notifs">
-    <span class="notifs-action">Tir cadré</span><br>
-    <span class="notifs-name-player">Gomis,</span> <span class="notifs-name-team">RC Lens</span>
+
+  <div class="notifications notifs-xy-b">
+    <div class="triangle-bg"></div>
+    <img class="shape-notifs" src="../assets/shapes/notifications/shape-team-b.svg" />
+    <div class="times-notifs color-team-b">
+      <span>75'</span>
+    </div>
+    <div class="info-notifs">
+      <span class="notifs-action">Tir cadré</span><br>
+      <span class="notifs-name-player">Gomis,</span> <span class="notifs-name-team">RC Lens</span>
+    </div>
   </div>
+
 </div>
